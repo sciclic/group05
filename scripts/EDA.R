2f1588650319e4aaee4ba8941fd3c8ed0e5dbbdb
Jacob Gerlofs <jgerlofs@mail.ubc.ca>
2020-03-23 12:31:39 -0700

    improving labels/titles on ggplots

--- scripts/EDA.R
+++ scripts/EDA.R
@@ -32,6 +32,7 @@ ggplot(survey_data, aes(current_living_location)) +
   coord_flip() +
   xlab("Continent") +
   ylab("Number of Graduate Students") +
+  ggtitle("Demographics of survey participants") +
   theme_minimal() +
   ggsave('basic_demographics1.png', path = here("images"), width = 8, height = 5)
 
@@ -40,8 +41,9 @@ survey_data %>%
   drop_na(level_of_satisfaction_with_decision_to_pursue_a_PhD, supervisor_relationship) %>%
   ggplot(aes(level_of_satisfaction_with_decision_to_pursue_a_PhD, supervisor_relationship)) +
   geom_point(position = "jitter", alpha = .12) +
-  xlab("Level of satisfaction with decision to pursue a PhD (1 - 5 scale)") +
-  ylab("Supervisor Relationship Satisfaction (1 - 7 scale") +
+  xlab("Level of satisfaction with decision to pursue a PhD \n (1 - 5 scale [lowest - highest])") +
+  ylab("Supervisor Relationship Satisfaction \n (1 - 7 scale [lowest - highest])") +
+  ggtitle("Self-reported levels of sastisfaction with PhD decisison vs \nsupervisor relationship satisfaction") +
   theme_minimal() +
   ggsave('satisfaction_v_supervis_relationship.png', path = here("images"), width = 8, height = 5)
 
@@ -50,8 +52,9 @@ survey_data %>%
   drop_na(level_of_satisfaction_with_decision_to_pursue_a_PhD, work_life_balance) %>%
   ggplot(aes(level_of_satisfaction_with_decision_to_pursue_a_PhD, work_life_balance)) +
   geom_point(position = "jitter", alpha = .12) +
-  xlab("Level of satisfaction with decision to pursue a PhD (1 - 5 scale)") +
-  ylab("Quality of work-life balance (1 - 7 scale)") +
+  xlab("Level of satisfaction with decision to pursue a PhD \n (1 - 5 scale [lowest - highest])") +
+  ylab("Quality of work-life balance \n (1 - 7 scale [lowest - highest])") +
+  ggtitle("Self-reported levels of sastisfaction with PhD career vs \nquality of work-life balance") +
   theme_minimal() +
   ggsave('satisfaction_v_work_life_bal.png', path = here("images"), width = 8, height = 5)
 
