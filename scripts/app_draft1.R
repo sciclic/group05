e8c6ead72bff7dff82e876eada12863734768df5
Jacob Gerlofs <jgerlofs@mail.ubc.ca>
2020-03-30 17:24:24 -0700

    added callback for satisfaction plot

--- scripts/app_draft1.R
+++ scripts/app_draft1.R
@@ -16,22 +16,6 @@ survey_data <- read.csv(here::here("data", "survey_data.csv"))
 
 # FUNCTIONS
 
-make_plot <- function(){
-  
-  # gets the label matching the column value
-  data <- survey_data
-  p <- ggplot(data, aes(x = level_of_satisfaction_with_decision_to_pursue_a_PhD, y = supervisor_relationship)) +
-    geom_jitter(alpha = 0.1) +
-    xlab("Satisfaction with decicsion to pursure a PhD") +
-    ylab("Quality of Supervisor Relationship") +
-    ggtitle("Self-reported sastifaction with decision to pursue a PhD vs <<VARIBALE>> \n (1 being lowest rating)") +
-    theme_minimal()
-  
-  # passing c("text") into tooltip only shows the contents of the "text" aesthetic specified above
-  ggplotly(p)
-}
-
-
 # Storing the labels/values as a tibble
 yaxisKey <- tibble(label = c("Quality of Supervisor Relationship", "Work-Life Balance", "Appropriateness of Mental Health Services for my Needs", "Amount to which University has long hour culture"),
                    value = c("supervisor_relationship", "work_life_balance", "mental_health_and_wellbeing_services_at_my_uni_are_appropriate_to_PhD_students_needs", "university_has_long_hours_culture"))
@@ -45,6 +29,26 @@ yaxisDropdown <- dccDropdown(
     }),
   value = "supervisor_relationship"
 )
+
+make_plot <- function(yaxis = "Quality of Supervisor Relationship"){
+  
+  # gets the label matching the column value
+  y_label <- yaxisKey$label[yaxisKey$value==yaxis]
+  
+  # gets the label matching the column value
+  data <- survey_data
+  p <- ggplot(data, aes(x = level_of_satisfaction_with_decision_to_pursue_a_PhD, y = !!sym(yaxis))) +
+    geom_jitter(alpha = 0.1) +
+    xlab("Satisfaction with decicsion to pursure a PhD") +
+    ylab(y_label) +
+    ggtitle(paste0("Self-reported sastifaction with decision to pursue a PhD vs ", y_label, " \n (1 being lowest rating)")) +
+    theme_minimal()
+  
+  # passing c("text") into tooltip only shows the contents of the "text" aesthetic specified above
+  ggplotly(p)
+}
+
+
 
 # "happiness variables" plots: satisfaction_now x
 ## 1 contact hours (x axis would be the different contact hours values, y axis is %, and then bars are coloured by level of satisfaction)
@@ -79,9 +83,18 @@ app$layout(
 )
 
 # CALLBACKS
+app$callback(
+  #update figure of gap-graph
+  output=list(id = 'satisfaction-graph', property='figure'),
+  #based on values of year, continent, y-axis components
+  params=list(input(id = 'y-axis', property='value')),
+  #this translates your list of params into function arguments
+  function(yaxis_value) {
+    make_plot(yaxis_value)
+  })
+
 
 # RUN APP
-
 app$run_server(debug=TRUE)
 
 # command to add dash app in Rstudio viewer:
