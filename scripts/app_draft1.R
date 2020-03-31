19ad27b14838debd49638b076f9906425cfaaaa9
Jacob Gerlofs <jgerlofs@mail.ubc.ca>
2020-03-30 17:10:25 -0700

    I got our first plot up and running!

--- scripts/app_draft1.R
+++ scripts/app_draft1.R
@@ -7,42 +7,35 @@ library(plotly)
 library(gapminder)
 
 # CREATE DASH INSTANCE
-
 app <- Dash$new()
 
+
 # LOAD IN DATASETS
-survey_data <- read.csv(here::here("data", "survey_dash.csv"))
+survey_data <- read.csv(here::here("data", "survey_data.csv"))
+
 
 # FUNCTIONS
-
-# plot
 
 make_plot <- function(){
   
   # gets the label matching the column value
-  
-  #filter our data based on the year/continent selections
   data <- survey_data
   p <- ggplot(data, aes(x = level_of_satisfaction_with_decision_to_pursue_a_PhD, y = supervisor_relationship)) +
     geom_jitter(alpha = 0.1) +
-    scale_color_manual(name = 'Continent', values = continent_colors) +
-    scale_x_continuous(breaks = unique(data$year))+
     xlab("Satisfaction with decicsion to pursure a PhD") +
     ylab("Quality of Supervisor Relationship") +
     ggtitle("Self-reported sastifaction with decision to pursue a PhD vs <<VARIBALE>> \n (1 being lowest rating)") +
-    theme_bw()
+    theme_minimal()
   
   # passing c("text") into tooltip only shows the contents of the "text" aesthetic specified above
-  ggplotly(p, 
-           tooltip = c("text"))
+  ggplotly(p)
 }
 
-## Assign components to variables
-# Storing the labels/values as a tibble means we can use this both 
-# to create the dropdown and convert colnames -> labels when plotting
 
-yaxisKey <- tibble(label = c("Quality of Supervisor Relationship", "Work-Life Balance", "Appropriateness of Mental Health Services for my Needs"),
-                   value = c("supervisor_relationship", "work_life_balance", "mental_health_and_wellbeing_services_at_my_uni_are_appropriate_to_PhD_students_needs"))
+# Storing the labels/values as a tibble
+yaxisKey <- tibble(label = c("Quality of Supervisor Relationship", "Work-Life Balance", "Appropriateness of Mental Health Services for my Needs", "Amount to which University has long hour culture"),
+                   value = c("supervisor_relationship", "work_life_balance", "mental_health_and_wellbeing_services_at_my_uni_are_appropriate_to_PhD_students_needs", "university_has_long_hours_culture"))
+
 #Create the dropdown
 yaxisDropdown <- dccDropdown(
   id = "y-axis",
@@ -52,12 +45,6 @@ yaxisDropdown <- dccDropdown(
     }),
   value = "supervisor_relationship"
 )
-
-graph <- dccGraph(
-  id = 'satisfaction-graph',
-  figure = make_plot() # gets initial data using argument defaults
-)
-
 
 # "happiness variables" plots: satisfaction_now x
 ## 1 contact hours (x axis would be the different contact hours values, y axis is %, and then bars are coloured by level of satisfaction)
@@ -70,9 +57,10 @@ graph <- dccGraph(
 heading_title <- htmlH1('Finding satisfaction in your PhD')
 heading_subtitle <- htmlH2('A data based approach to the sources of satisfaction in graduate school')
 
-# DROPDOWNS
-
-# MARKDOWNS
+graph <- dccGraph(
+  id = 'satisfaction-graph',
+  figure = make_plot() # gets initial data using argument defaults
+)
 
 # SPECIFY APP LAYOUT
 
@@ -82,8 +70,10 @@ app$layout(
       heading_title,
       heading_subtitle,
       #graphs and markdown components
+      htmlLabel('Select predictor of satisfaction:'),
+      yaxisDropdown,
       graph,
-      dropdown
+      htmlIframe(height=30, width=6, style=list(borderWidth = 0))
     )
   )
 )
